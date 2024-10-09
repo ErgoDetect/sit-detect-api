66898e26d588caa9e05b2fb497c38a8902fcc086
Sealed <korn2400@gmail.com>
2024-10-10 02:34:34 +0700

    fix respond counter and adjust value of detect condition

--- api/routes/websocket_router.py
+++ api/routes/websocket_router.py
@@ -15,8 +15,8 @@ async def landmark_results(websocket: WebSocket):
 
     # Constants
     frame_per_second = 1
-    ear_threshold_low = 0.2
-    ear_threshold_high = 0.4
+    ear_threshold_low = 0.4
+    ear_threshold_high = 0.5
 
     # Initialization of variables
     response_counter = 0
@@ -50,7 +50,6 @@ async def landmark_results(websocket: WebSocket):
             message = await websocket.receive_text()
             object_data = json.loads(message)
             processed_data = processData(object_data['data'])
-
             # Extract current values
             current_values = {
                 "shoulderPosition": processed_data.get_shoulder_position(),
@@ -61,9 +60,9 @@ async def landmark_results(websocket: WebSocket):
             }
 
             # Process the first 5 messages to establish baseline values
-            if response_counter < 5:
+            if response_counter <= 5:
                 saved_values.append(current_values) 
-                response_counter += 1
+                # response_counter += 1
                 if response_counter == 5:
                     # Compute averages for correct_values
                     def average(values):
@@ -75,134 +74,126 @@ async def landmark_results(websocket: WebSocket):
                         "diameterRight": average([v['diameterRight'] for v in saved_values]),
                         "diameterLeft": average([v['diameterLeft'] for v in saved_values])
                     }
-                continue  # Skip further processing until baseline is established
-
-            
-
-            current_values= {"shoulderPosition":processed_data.get_shoulder_position(),
-                            "diameterRight":processed_data.get_diameter_right(),
-                            "diameterLeft":processed_data.get_diameter_left(),
-                            "eyeAspectRatioRight":processed_data.get_blink_right(),
-                            "eyeAspectRatioLeft":processed_data.get_blink_left()}
-            
-            if current_values["shoulderPosition"] == None:
-                thoracic_stack = 0
-            elif correct_values["shoulderPosition"]*0.90<=current_values["shoulderPosition"]:
-                thoracic_stack += 1
-            else : thoracic_stack = 0
-
-            # Update thoracic_stack if necessary
-            if current_values["shoulderPosition"] == None:
-                thoracic_stack = 0
-            elif correct_values["shoulderPosition"]*0.90<=current_values["shoulderPosition"]:
-                thoracic_stack += 1
-            else : thoracic_stack = 0
-
-            # Reset stacks if no face is detected
-            if object_data['data']["faceDetect"] == False:
-                blink_stack = 0
-                sitting_stack = 0
-                distance_stack = 0
             else:
-                sitting_stack += 1
+                current_values= {"shoulderPosition":processed_data.get_shoulder_position(),
+                                "diameterRight":processed_data.get_diameter_right(),
+                                "diameterLeft":processed_data.get_diameter_left(),
+                                "eyeAspectRatioRight":processed_data.get_blink_right(),
+                                "eyeAspectRatioLeft":processed_data.get_blink_left()}
 
-                # Update distance_stack
-                diameter_right = current_values.get("diameterRight")
-                diameter_left = current_values.get("diameterLeft")
-                correct_diameter_right = correct_values.get("diameterRight")
-                correct_diameter_left = correct_values.get("diameterLeft")
+                # Update thoracic_stack if necessary
+                if current_values["shoulderPosition"] == None:
+                    thoracic_stack = 0
+                elif correct_values["shoulderPosition"]+0.05<=current_values["shoulderPosition"]:
+                    thoracic_stack += 1
+                else : thoracic_stack = 0
 
-                latest_nearest_distance = max(
-                    diameter_right or latest_nearest_distance,
-                    diameter_left or latest_nearest_distance
-                )
-
-                correct_distance = max(
-                    correct_diameter_right or 0, correct_diameter_left or 0
-                )
-
-                if correct_distance and latest_nearest_distance:
-                    if correct_distance * 0.90 <= latest_nearest_distance:
-                        distance_stack += 1
-                    else:
-                        distance_stack = 0
-
-                # Update blink_stack
-                ear_left = current_values.get("eyeAspectRatioLeft")
-                ear_right = current_values.get("eyeAspectRatioRight")
-
-                if ((ear_left is not None and ear_left <= ear_threshold_low) or 
-                   (ear_right is not None and ear_right <= ear_threshold_low)):
-                    ear_below_threshold = True
-                    blink_stack += 1
-                elif ear_below_threshold and (
-                    (ear_left is not None and ear_left >= ear_threshold_high) or
-                    (ear_right is not None and ear_right >= ear_threshold_high)):
-                    if not blink_detected:
-                        blink_stack = 0
-                        blink_detected = True
-                    ear_below_threshold = False
+                # Reset stacks if no face is detected
+                if object_data['data']["faceDetect"] == False:
+                    blink_stack = 0
+                    sitting_stack = 0
+                    distance_stack = 0
                 else:
-                    blink_detected = False
-                    blink_stack += 1
-            
-            if(blink_stack >= blink_stack_threshold * frame_per_second ):
-                if(result[0]==False):
-                    timeline_result["blink"].append([]) 
-                    timeline_result["blink"][len(timeline_result["blink"])-1].append(response_counter-blink_stack_threshold)
-                result[0] = True
-            else:
-                if(result[0]==True):
-                    timeline_result["blink"][len(timeline_result["blink"])-1].append(response_counter)
-                result[0] = False
+                    sitting_stack += 1
 
-            if(sitting_stack >= sitting_stack_threshold * frame_per_second ):
-                if(result[1]==False):
-                    timeline_result["sitting"].append([]) 
-                    timeline_result["sitting"][len(timeline_result["sitting"])-1].append(response_counter-sitting_stack_threshold)
-                result[1] = True
-            else:
-                if(result[1]==True):
-                    timeline_result["sitting"][len(timeline_result["sitting"])-1].append(response_counter)
-                result[1] = False
+                    # Update distance_stack
+                    diameter_right = current_values.get("diameterRight")
+                    diameter_left = current_values.get("diameterLeft")
+                    correct_diameter_right = correct_values.get("diameterRight")
+                    correct_diameter_left = correct_values.get("diameterLeft")
 
-            if(distance_stack >= distance_stack_threshold * frame_per_second ):
-                if(result[2]==False):
-                    timeline_result["distance"].append([]) 
-                    timeline_result["distance"][len(timeline_result["distance"])-1].append(response_counter-distance_stack_threshold)
-                result[2] = True
-            else:
-                if(result[2]==True):
-                    timeline_result["distance"][len(timeline_result["distance"])-1].append(response_counter)
-                result[2] = False
+                    latest_nearest_distance = max(
+                        diameter_right or latest_nearest_distance,
+                        diameter_left or latest_nearest_distance
+                    )
 
-            if(thoracic_stack >= thoracic_stack_threshold * frame_per_second ):
-                if(result[3]==False):
-                    timeline_result["thoracic"].append([]) 
-                    timeline_result["thoracic"][len(timeline_result["thoracic"])-1].append(response_counter-thoracic_stack_threshold)
-                result[3] = True
-            else:
-                if(result[3]==True):
-                    timeline_result["thoracic"][len(timeline_result["thoracic"])-1].append(response_counter)
-                result[3] = False
+                    correct_distance = max(
+                        correct_diameter_right or 0, correct_diameter_left or 0
+                    )
 
-            # print(timeline_result)
-            # อาจต้องแก้ตัวนับ response เพราะตอนนี้มันติด continue ข้างบน เลยใช้ท่านี้แก้ขัดไปก่อน
-            if(response_counter >=5):
-                response_counter += 1
+                    if correct_distance and latest_nearest_distance:
+                        if correct_distance * 0.90 <= latest_nearest_distance:
+                            distance_stack += 1
+                        else:
+                            distance_stack = 0
+
+                    # Update blink_stack
+                    ear_left = current_values.get("eyeAspectRatioLeft")
+                    ear_right = current_values.get("eyeAspectRatioRight")
+
+                    if ((ear_left is not None and ear_left <= ear_threshold_low) or 
+                    (ear_right is not None and ear_right <= ear_threshold_low)):
+                        ear_below_threshold = True
+                        blink_stack += 1
+                    elif ear_below_threshold and (
+                        (ear_left is not None and ear_left >= ear_threshold_high) or
+                        (ear_right is not None and ear_right >= ear_threshold_high)):
+                        if not blink_detected:
+                            blink_stack = 0
+                            blink_detected = True
+                        ear_below_threshold = False
+                    else:
+                        blink_detected = False
+                        blink_stack += 1
                 
-            # Send the result back to the client
-            await websocket.send_json({
-                "blink_alert": result[0],
-                "sitting_alert": result[1],
-                "distance_alert": result[2],
-                "thoracic_alert": result[3]
-            })
-            # print("blink:",blink_stack)
-            # print("sitting:",sitting_stack)
-            # print("distance:",distance_stack)
-            # print("thoracic:",thoracic_stack)
+                if(blink_stack >= blink_stack_threshold * frame_per_second ):
+                    if(result[0]==False):
+                        timeline_result["blink"].append([]) 
+                        timeline_result["blink"][len(timeline_result["blink"])-1].append(response_counter-blink_stack_threshold)
+                    result[0] = True
+                else:
+                    if(result[0]==True):
+                        timeline_result["blink"][len(timeline_result["blink"])-1].append(response_counter)
+                    result[0] = False
 
+                if(sitting_stack >= sitting_stack_threshold * frame_per_second ):
+                    if(result[1]==False):
+                        timeline_result["sitting"].append([]) 
+                        timeline_result["sitting"][len(timeline_result["sitting"])-1].append(response_counter-sitting_stack_threshold)
+                    result[1] = True
+                else:
+                    if(result[1]==True):
+                        timeline_result["sitting"][len(timeline_result["sitting"])-1].append(response_counter)
+                    result[1] = False
+
+                if(distance_stack >= distance_stack_threshold * frame_per_second ):
+                    if(result[2]==False):
+                        timeline_result["distance"].append([]) 
+                        timeline_result["distance"][len(timeline_result["distance"])-1].append(response_counter-distance_stack_threshold)
+                    result[2] = True
+                else:
+                    if(result[2]==True):
+                        timeline_result["distance"][len(timeline_result["distance"])-1].append(response_counter)
+                    result[2] = False
+
+                if(thoracic_stack >= thoracic_stack_threshold * frame_per_second ):
+                    if(result[3]==False):
+                        timeline_result["thoracic"].append([]) 
+                        timeline_result["thoracic"][len(timeline_result["thoracic"])-1].append(response_counter-thoracic_stack_threshold)
+                    result[3] = True
+                else:
+                    if(result[3]==True):
+                        timeline_result["thoracic"][len(timeline_result["thoracic"])-1].append(response_counter)
+                    result[3] = False
+
+                # print(timeline_result)
+
+                # Send the result back to the client
+                await websocket.send_json({
+                    "blink_alert": result[0],
+                    "sitting_alert": result[1],
+                    "distance_alert": result[2],
+                    "thoracic_alert": result[3]
+                })
+
+                # print("correct:",correct_values)
+                # print("current:",current_values)
+                # print("blink:",blink_stack)
+                # print("sitting:",sitting_stack)
+                # print("distance:",distance_stack)
+                # print("thoracic:",thoracic_stack)
+                # print("counter:",response_counter)
+            response_counter += 1
            
 
     except WebSocketDisconnect:
